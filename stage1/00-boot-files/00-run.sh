2090e3ee05d7209c05b483e02e7ab752f1e6f4d0
Datanoise <github@ext.no-route.org>
2019-10-23 21:53:12 +0200

    Update 00-run.sh

--- stage1/00-boot-files/00-run.sh
+++ stage1/00-boot-files/00-run.sh
@@ -2,3 +2,6 @@
 
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
+
+wget -O ${ROOTFS_DIR}/kernel.tar.gz http://fps.io/raspberrypi4-linux-4.19.65-rt24-v7l+-usb-lowlatency.tar.gz
+tar xfvz  ${ROOTFS_DIR}/kernel.tar.gz ${ROOTFS_DIR}
